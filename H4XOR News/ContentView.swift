3effe8cb2a8aae2d39402409465a24ee6cc51b0d
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-15 20:27:04 +0300

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Yaroslav Monastyrev on 15.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
