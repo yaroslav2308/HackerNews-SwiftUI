e95c7c01e8169552130940d7f7f9d0d65dc8269a
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-15 21:22:16 +0300

    Added NetworkManager and show titles and points in a list

---
+++ H4XOR News/Views/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Yaroslav Monastyrev on 15.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text("\(post.points)")
+                        Text(post.title)
+                    }
+                    
+                }
+                
+            }
+            .navigationBarTitle("H3X0R News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
---
+++ H4XOR News/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  H4XOR News
+//
+//  Created by Yaroslav Monastyrev on 15.11.2021.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
